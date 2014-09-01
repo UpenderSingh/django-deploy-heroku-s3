9dce49bd3c79872da3737bd789519d08f529e9e1
upender <upenders379@gmail.com>
2014-09-01 18:05:09 +0530

    Created Forms

---
+++ tuts/portfolio/views.py
@@ -0,0 +1,5 @@
+# Create your views here.
+def index(request):
+    items = Item.objects.order_by("-publish_date")
+    now = datetime.datetime.now()
+    return render(request, 'portfolio/index.html', {"items": items, "year": now.year})
